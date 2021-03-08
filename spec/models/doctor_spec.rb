87e757a7c03735f299039131e2f49c92d21b9231
Diana Buffone <diana.pamelaet@gmail.com>
2021-03-08 11:22:33 -0500

    Pass relationship tests

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Doctor, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should have_many :doctor_surgeries}
+    it { should have_many(:surgeries).through(:doctor_surgeries)}
+  end
 end
